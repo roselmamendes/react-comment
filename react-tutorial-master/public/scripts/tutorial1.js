39f90996651d40944fa57a9bcd15497d667d35c8
Roselma Mendes <roselma.mendes@gmail.com>
2015-12-27 20:25:26 -0300

    Add Comment component.

--- react-tutorial-master/public/scripts/tutorial1.js
+++ react-tutorial-master/public/scripts/tutorial1.js
@@ -7,28 +7,44 @@ var CommentBox = React.createClass({
 				<CommentForm />
 			</div>
 			);
-	};
+	}
 });
 
 var CommentList = React.createClass({
 	render: function() {
 		return (
 			<div className="commentList">
-			 Hello, world! I am a CommentList.
+				<Comment author="Pete Hunt">This is one comment</Comment>
+				<Comment author="Jordan Walke">This is *another* comment</Comment>
 			</div>
 		);
-	};
+	}
+});
+
+var Comment = React.createClass({
+	render: function() {
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{marked(this.props.children.toString())}
+			</div>
+		);
+	}
 });
 
 var CommentForm = React.createClass({
- render: function() {
-	 <div className="commentForm">
-		 Hello, world! I am a CommentForm.
-	</div>
- };
+	render: function() {
+	 return (
+		<div className="commentForm">
+		 	Hello, world! I am a CommentForm.
+		</div>
+			);
+	}
 });
 
-ReactDom.render(
+ReactDOM.render(
 	<CommentBox />,
 	document.getElementById('content')
 );

9d61482092914acb259b32a34102a4d53525e2f7
Roselma Mendes <roselma.mendes@gmail.com>
2015-12-27 20:42:20 -0300

    Use a json data instead a hard code for comments.

--- react-tutorial-master/public/scripts/tutorial1.js
+++ react-tutorial-master/public/scripts/tutorial1.js
@@ -1,9 +1,15 @@
+var data = [
+	{id: 1, author: "Mauricio Melo", text: "Eu sou o Mauricio."},
+	{id: 2, author: "Raony Lima", text: "Eu sou o Raony."},
+];
+
+
 var CommentBox = React.createClass({
 	render: function() {
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList />
+				<CommentList  data={this.props.data}/>
 				<CommentForm />
 			</div>
 			);
@@ -12,23 +18,35 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
 	render: function() {
+		var commentNodes = this.props.data.map(function(comment) {
+			return (
+				<Comment author={comment.author} key={comment.id}>
+					{comment.text}
+				</Comment>
+			);
+		});
+		
 		return (
 			<div className="commentList">
-				<Comment author="Pete Hunt">This is one comment</Comment>
-				<Comment author="Jordan Walke">This is *another* comment</Comment>
+				{commentNodes}
 			</div>
 		);
 	}
 });
 
 var Comment = React.createClass({
+	rawMarkup: function() {
+		var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+		return {__html: rawMarkup};
+	},
+	
 	render: function() {
 		return (
 			<div className="comment">
 				<h2 className="commentAuthor">
 					{this.props.author}
 				</h2>
-				{marked(this.props.children.toString())}
+				<span dangerouslySetInnerHTML = {this.rawMarkup()} />
 			</div>
 		);
 	}
@@ -45,6 +63,6 @@ var CommentForm = React.createClass({
 });
 
 ReactDOM.render(
-	<CommentBox />,
+	<CommentBox data={data}/>,
 	document.getElementById('content')
 );
